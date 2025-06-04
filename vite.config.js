e29c8acf12b5b514e43a07257c37adcd130c38b7
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2025-06-04 21:11:08 +0400

    force add built vite assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'public/build/assets/app-B-bVuAmC.css', 'resources/js/app.js', 'public/build/assets/app-rYFCqwdu.js'],
@@ -10,4 +11,14 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: [
+                'resources/js/app.js',
+                'resources/css/app.css',
+            ]
+        },
+    }
 });
